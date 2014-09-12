68d9ce205702aef2fbd4e4a56321cd69d6ac0ee9
stevenlanders <steven.landers@gmail.com>
2014-09-12 13:23:39 -0400

    updating web project

--- BlockPuzzleSolverAPI/src/main/java/com/stevenlanders/blockpuzzle/controller/BlockPuzzleController.groovy
+++ BlockPuzzleSolverAPI/src/main/java/com/stevenlanders/blockpuzzle/controller/BlockPuzzleController.groovy
@@ -1,22 +1,45 @@
 package com.stevenlanders.blockpuzzle.controller
 
-import org.springframework.stereotype.Controller
+import com.stevenlanders.blockpuzzle.BlockPuzzle
+import com.stevenlanders.blockpuzzle.BlockPuzzleUtility
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Created by stevenlanders on 9/12/14.
  */
 
-@Controller
+@RestController
 @RequestMapping("/blockpuzzle")
 class BlockPuzzleController {
 
+    private static final int DEFAULT_PUZZLE_SIZE = 4
+
     @RequestMapping("/solution")
-    def String getSolution(@RequestParam String puzzle){
+    def getSolution(@RequestParam String puzzle){
+        try{
+            BlockPuzzle blockPuzzle = BlockPuzzleUtility.parsePuzzleString(puzzle)
+            blockPuzzle.solve()
+            def moves = blockPuzzle.getMoves()
+            [
+                    moves: moves
+            ]
+        }catch(IllegalArgumentException iae){
+            return "invalid"
+        }
+    }
 
-        throw new UnsupportedOperationException("not implemented")
+    @RequestMapping(["/",""])
+    def getPuzzleDefault(){
+        return getPuzzle(DEFAULT_PUZZLE_SIZE)
+    }
 
+    @RequestMapping("/{size}")
+    def getPuzzle(@PathVariable Integer size){
+        BlockPuzzle blockPuzzle = BlockPuzzle.generate(size).shuffle()
+        return [puzzle: blockPuzzle.toString()]
     }
 
 }
